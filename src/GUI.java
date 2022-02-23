1b3f458a98b41178ce31d2fac1be3425c5b2212d
monwe-jr <monwefrancis@gmail.com>
2022-02-23 17:10:00 -0500

    Test push

---
+++ src/GUI.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class GUI {
+    JFrame frame;
+    JPanel panel;
+    TrafficLight light1;
+    Segment road1;
+    Intersection intersect1;
+
+    GUI(){
+
+    }
+
+
+
+//GUI generation
+    private void createGUI(){
+
+    }
+
+//updates the simulation GUI
+    static public void updateGUI(){
+
+    }
+
+
+}
---
+++ src/Truck.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+
+public class Truck extends Vehicle {
+
+
+    Truck(Color c, boolean driveable) {
+        super(c, driveable);
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void acceleration() {
+
+    }
+}
